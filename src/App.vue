bbf15485c1d264ce345c82d0b94e9264ee96efd1
Denis <dengalai20@gmail.com>
2022-05-30 01:34:18 +0300

    First

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>SteveDog huesos!</div>
+  <n-message-provider>
+    <n-form
+        ref="formRef"
+        inline
+        :label-width="80"
+    >
+
+      <div class="form-values">
+        <n-form-item label="Город:" path="city">
+          <n-auto-complete
+              :on-update:value="onUpdated"
+              :options="options"
+              placeholder="Город"
+          />
+        </n-form-item>
+        <n-form-item label="Название улицы:" path="street">
+          <n-input v-model:value="formData.street" placeholder="Улица"/>
+        </n-form-item>
+        <n-form-item label="Номер дома:" path="number">
+          <n-input v-model:value="formData.number" placeholder="Номер дома"/>
+        </n-form-item>
+        <n-form-item label="Номер квартиры:" path="numberApart">
+          <n-input v-model:value="formData.numberApart" placeholder="Номер квартиры"/>
+        </n-form-item>
+        <n-form-item>
+          <n-button @click="sendData">
+            Додати мою адресу
+          </n-button>
+        </n-form-item>
+      </div>
+    </n-form>
+
+    <pre>{{ JSON.stringify(options, null, 2) }}</pre>
+  </n-message-provider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent} from "vue";
+import axios from "axios";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+      name: 'App',
+      data() {
+        return {
+          formData: {
+            city: "",
+            street: "",
+            number: "",
+            numberApart: ""
+          },
+          options: [],
+        }
+      },
+      methods: {
+        onUpdated(text) {
+          axios.get("http://localhost:8080/location/city?value=" + text).then(res => { // Запрос данных с бека
+            this.options = res.data;
+          })
+        },
+        sendData() {
+          /*window.Telegram.WebApp.sendData("qweQEW");*/
+          axios.post("http://localhost:8080/location/city?value=", this.formData) // Отправка данных на бек
+        }
+      },
+    }
+)
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'vfonts/Lato.css';
+@import 'vfonts/FiraCode.css';
+
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.form-values {
+  display: flex;
+  flex-direction: column;
 }
 </style>
