fd21e000c27f85e9cf5bad1d97c63eb5066aa285
Denis <dengalai20@gmail.com>
2022-07-10 10:34:35 +0300

    New ngrok link

--- src/components/AllCases.vue
+++ src/components/AllCases.vue
@@ -77,7 +77,7 @@ export default defineComponent({
     }
   },
   created() {
-    axios.get("https://f7b3-93-170-55-154.eu.ngrok.io/allcases?userId=" + this.userId).then(res => {
+    axios.get("https://f0fa-93-170-55-154.eu.ngrok.io/allcases?userId=" + this.userId).then(res => {
       this.data = res.data.map(item => {
         item.date = DateTime.fromISO(item.date).toFormat('HH:mm, dd LLL yyyy');
         return item;
