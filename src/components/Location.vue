65b2577ac61893a93e565041a0ab3aed9876838c
Denis <dengalai20@gmail.com>
2022-07-10 22:31:28 +0300

    New ngrok link

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -60,18 +60,18 @@ export default defineComponent({
   methods: {
     onUpdatedCity(text) {
       this.formData.city = text;
-      axios.get("http://93.170.55.154/location/?cityName=" + text).then(res => { // Запрос данных с бека
+      axios.get("https://bot-test.fun/location/?cityName=" + text).then(res => { // Запрос данных с бека
         this.cityOptions = res.data; // Ответ на фронт
       })
     },
     onUpdatedStreet(text) {
       this.formData.street = text;
-      axios.get("http://93.170.55.154/location/?cityName=" + this.formData.city + "&streetName=" + text).then(res => { // Запрос данных с бека
+      axios.get("https://bot-test.fun/location/?cityName=" + this.formData.city + "&streetName=" + text).then(res => { // Запрос данных с бека
         this.streetOptions = res.data; // Ответ на фронт
       })
     },
     onGetCoordinates(lat, lng) {
-      axios.get("http://93.170.55.154/location/auto/?lat=" + lat + "&lng=" + lng).then(res => {
+      axios.get("https://bot-test.fun/location/auto/?lat=" + lat + "&lng=" + lng).then(res => {
         this.formData.city = res.data.city;
         this.formData.street = res.data.street;
         this.formData.number = res.data.number;
