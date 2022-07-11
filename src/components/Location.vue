b6911804a6bcbf72e8829fb24ac71c0eef8d451b
Denis <dengalai20@gmail.com>
2022-07-11 16:16:14 +0300

    Add region field

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -6,6 +6,14 @@
         :label-width="80"
     >
       <div class="form-values">
+        <n-form-item label="Область:" path="city">
+          <n-auto-complete
+              :value="formData.region"
+              :on-update:value="onUpdatedRegion"
+              :options="regionOptions"
+              placeholder="Область"
+          />
+        </n-form-item>
         <n-form-item label="Город:" path="city">
           <n-auto-complete
               :value="formData.city"
@@ -48,30 +56,39 @@ export default defineComponent({
   data() {
     return {
       formData: {
+        region: "",
         city: "",
         street: "",
         number: "",
         numberApart: ""
       },
+      regionOptions: [],
       cityOptions: [],
       streetOptions: [],
     }
   },
   methods: {
+    onUpdatedRegion(text) {
+      this.formData.region = text;
+      axios.get("https://bot-test.fun/location/?regionName=" + text).then(res => { // Запрос данных с бека
+        this.regionOptions = res.data; // Ответ на фронт
+      })
+    },
     onUpdatedCity(text) {
       this.formData.city = text;
-      axios.get("https://bot-test.fun/location/?cityName=" + text).then(res => { // Запрос данных с бека
+      axios.get("https://bot-test.fun/location/?regionName=" + this.regionOptions + "&cityName=" + text).then(res => { // Запрос данных с бека
         this.cityOptions = res.data; // Ответ на фронт
       })
     },
     onUpdatedStreet(text) {
       this.formData.street = text;
-      axios.get("https://bot-test.fun/location/?cityName=" + this.formData.city + "&streetName=" + text).then(res => { // Запрос данных с бека
+      axios.get("https://bot-test.fun/location/?regionName=" + this.regionOptions + "&cityName=" + this.formData.city + "&streetName=" + text).then(res => { // Запрос данных с бека
         this.streetOptions = res.data; // Ответ на фронт
       })
     },
     onGetCoordinates(lat, lng) {
       axios.get("https://bot-test.fun/location/auto/?lat=" + lat + "&lng=" + lng).then(res => {
+        this.formData.region = res.data.region;
         this.formData.city = res.data.city;
         this.formData.street = res.data.street;
         this.formData.number = res.data.number;
