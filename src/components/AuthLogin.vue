819aa7cb5a5f7f9f866539257943fec2cc7a2b18
Denis <dengalai20@gmail.com>
2022-07-31 12:00:36 +0300

    Delete auto location

---
+++ src/components/AuthLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthLogin',
+  props: {
+    msg: String
+  },
+  mounted() {
+    const insertScript = document.createElement('script')
+    insertScript.async
+    insertScript.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?19')
+    insertScript.setAttribute('data-telegram-login', 'company_testDenis_bot')
+    insertScript.setAttribute('data-size', 'large')
+    insertScript.setAttribute('data-onauth', 'onTelegramAuth(user)')
+    document.body.appendChild(insertScript)
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import naive from 'naive-ui'
 import VueGeolocation from "vue3-geolocation/src";
 import { createRouter, createWebHistory } from 'vue-router'
 import Location from "@/components/Location";
-import Auth from "@/components/Auth";
+import AuthLogin from "@/components/AuthLogin";
 import AllCases from "@/components/AllCases";
 import CreateCase from "@/components/CreateCase";
 import AllLocation from "@/components/AllLocation";
 
 const routes = [
-    {path: '/', component: Auth},
+    {path: '/', component: AuthLogin},
     {path: '/location', component: Location},
     {path: '/allcases', component: AllCases},
     {path: '/createcase', component: CreateCase},
