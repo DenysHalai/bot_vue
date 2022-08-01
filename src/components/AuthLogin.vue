572e08899e9b31c0e8a362ce3cee7fee989f82d9
Denis <dengalai20@gmail.com>
2022-08-01 11:40:02 +0300

    Delete auto location

--- src/components/AuthLogin.vue
+++ src/components/AuthLogin.vue
@@ -1,39 +1,75 @@
 <template>
-  <div>
-    Тут будет форма<br>
-    Pole 1 <br/>
-    Pole 2 <br/><br/>
-    Pole 3 <br/><br/>
-    Кнопка авторизации<br/>
+  <div class="container">
+
+    <!-- Callback mode -->
+    <!--
+    -->
+    <vue-telegram-login
+        v-if="userInfo == null"
+        mode="callback"
+        telegram-login="company_testDenis_bot"
+        @callback="login" />
+
+    <b-card
+        v-else
+        :title="userInfo.username"
+        :img-src="userInfo.photo_url"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2">
+
+      <b-button variant="danger" @click="logout">Logout</b-button>
+    </b-card>
+
+    <!-- Redirect mode -->
+    <!--
+    <vue-telegram-login
+       mode="redirect"
+       telegram-login="belajarwidgetbot"
+       redirect-url="https://k3qn0kznov.codesandbox.io/" />
+    -->
   </div>
+
+
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
+import { vueTelegramLogin } from "vue-telegram-login";
+
 export default {
-  name: 'AuthLogin',
-  props: {
-    msg: String
+  name: "AuthLogin",
+  components: { vueTelegramLogin },
+  data (){
+    return {
+      userInfo : ''
+    }
   },
-  mounted() {
-    const insertScript = document.createElement('script')
-    insertScript.async
-    insertScript.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?19')
-    insertScript.setAttribute('data-telegram-login', 'company_testDenis_bot')
-    insertScript.setAttribute('data-size', 'large')
-    insertScript.setAttribute('data-onauth', 'onTelegramAuth(user)')
-    insertScript.setAttribute('data-request-access', 'write')
-    insertScript.setAttribute('data-radius', '1')
-    document.body.appendChild(insertScript)
+  mounted(){
+    this.getUser();
   },
-  methods:{
-    onTelegramAuth(user) {
-      alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-      console.log('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')')
+  methods: {
+    login(user) {
+
+      // this.userInfo = user
+      this.$cookies.set('usertg',user);
+      this.getUser();
+
+
+      // gets user as an input
+      // id, first_name, last_name, username,
+      // photo_url, auth_date and hash
+      // console.log(user);
+    },
+    getUser(){
+      this.userInfo = this.$cookies.get('usertg')
+    },
+    logout(){
+      // console.log('remove')
+      this.$cookies.remove('usertg');
+      this.getUser()
     }
   }
-}
+};
 </script>
-
-<style scoped>
-</style>
